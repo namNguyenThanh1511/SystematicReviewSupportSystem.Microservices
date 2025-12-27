d58594d824666a72922e84cb7ce284db924c3aa0
AmayaKuro <vhtkiet9615@gmail.com>
2025-12-27 23:02:08 +0700

    clean code settings object

--- src/Services/SRSS.IAM/SRSS.IAM.API/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
+++ src/Services/SRSS.IAM/SRSS.IAM.API/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -25,7 +25,7 @@ namespace SRSS.IAM.API.DependencyInjection.Extensions
 
             services.AddSignalR();
             services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
-            services.Configure<GoogleAuthSettings>(configuration.GetSection("GoogleAuthSettings"));
+            services.Configure<GoogleAuthSettings>(configuration.GetSection(GoogleAuthSettings.SectionName));
             // ✅ Register repositories first
             services.AddScoped<IRedisCacheService, RedisCacheService>();
             services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
